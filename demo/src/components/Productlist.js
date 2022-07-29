ca35fe49f699fca76f87c9460ea8f3beb304ead5
ashokpkd <102400169+ashokpkd@users.noreply.github.com>
2022-07-29 15:06:02 +0530

    Add files via upload

---
+++ demo/src/components/Productlist.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Productlist(props) {
+  return (
+
+    <tr>
+    <td>{props.productname}</td>
+    <td>{props.productprice}</td>
+    <td>{props.quantity}</td>
+    <td>{props.category}</td>
+   </tr>
+   
+  )
+}
+
+export default Productlist
